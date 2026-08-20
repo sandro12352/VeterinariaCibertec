e879c9b51e76b6ec7fef12b6b18a74f5a4b4a7d5
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2026-08-19 23:55:55 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'auth/login',
+        component:LoginComponent,
+    },
+    {
+        path:'**',
+        redirectTo:'auth/login',
+        pathMatch:'full',
+    }
+];
