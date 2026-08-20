e1c73fb48897cbf870f31e9bd0ed95a3e36f57bd
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2026-08-20 00:10:58 -0500

    agregando login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  email = '';
+  password = '';
 
+  rememberMe = false;
+  showPassword = false;
+  loading = false;
+
+  login(): void {
+
+    if (!this.email || !this.password) {
+      return;
+    }
+
+    this.loading = true;
+
+    // Aquí irá tu AuthService
+    console.log({
+      email: this.email,
+      password: this.password,
+      rememberMe: this.rememberMe
+    });
+
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
+  }
 }
