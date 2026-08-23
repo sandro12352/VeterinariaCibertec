fde4cc351a045a89012a68227bf7ce320ec3b26a
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2026-08-22 20:43:04 -0500

    cambios top bar

--- src/app/shared/components/topbar/topbar.component.ts
+++ src/app/shared/components/topbar/topbar.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css',
 })
 export class TopbarComponent {
+  private readonly authService = inject(AuthService);
+
+  obtenerNombre() {
+    return sessionStorage.getItem('authUsername')
+  }
+
+  obtenerRol(): string {
+    const rawRoles = sessionStorage.getItem('authRoles');
+    if (!rawRoles) return '';
+
+    try {
+      const roles: string[] = JSON.parse(rawRoles);
+      if (roles.includes('ROLE_ADMIN')) {
+        return 'Administrador';
+      }
+       if (roles.includes('ROLE_VETERINARIO')) {
+        return 'Veterinario';
+      }
+    } catch (e) {
+      return '';
+    }
+
+    return '';
+  }
 
 }
